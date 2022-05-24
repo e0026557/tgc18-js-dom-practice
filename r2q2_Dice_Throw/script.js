0e82c94b527e3a32c6ab803d1acce1db8371591d
Wesley Cheah <wesley_188@yahoo.com>
2022-05-24 09:17:04 +0000

    Completed r2q2

--- r2q2_Dice_Throw/script.js
+++ r2q2_Dice_Throw/script.js
@@ -1,3 +1,19 @@
 // PLACE YOUR CODE HERE
+function getRandomInt() {
+    return Math.floor(Math.random()*5)+1;
+}
 
+let button = document.querySelector('button');
+button.addEventListener('click', function () {
+    let boxes = document.querySelectorAll('.box');
+    let sum = 0;
+    for (let box of boxes) {
+        let value = getRandomInt();
+        box.innerHTML = value;
+        sum += value;
+    }
+
+    let result = document.querySelector('span');
+    result.innerHTML = sum;
+})
 
