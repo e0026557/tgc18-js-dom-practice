2f3da5611438ff7f86b5e9698998d08d90542ed5
Wesley Cheah <wesley_188@yahoo.com>
2022-05-24 07:26:30 +0000

    Completed q4

---
+++ q4_more_manipulation/script.js
@@ -0,0 +1,23 @@
+// Q1
+document.querySelector('h1').style.color = 'green';
+
+// Q2
+document.querySelector('li.finished').innerText = 'Repay credit card debt';
+
+// Q3
+document.querySelector('#urgent').style.border = '2px solid black';
+
+// Q4
+document.querySelector('.emphasis').style.textDecoration = 'underline';
+
+// Q5
+let pEmphasis = document.querySelector('p.emphasis');
+pEmphasis.style.backgroundColor = 'orange';
+pEmphasis.style.fontSize = '32px';
+pEmphasis.style.lineHeight = '24px';
+
+// Q6
+document.querySelector('#urgent span.todo').style.backgroundColor = 'yellow';
+
+// Q7
+document.querySelector('h2').innerHTML = '<span class="greetings">About Us</span>';
