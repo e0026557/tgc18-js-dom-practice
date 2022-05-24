3337cf5576c61ebb503aeab98d43f7cc115c84fd
Wesley Cheah <wesley_188@yahoo.com>
2022-05-24 08:19:49 +0000

    Completed q8

--- q8_manipulating_dom_elements/script.js
+++ q8_manipulating_dom_elements/script.js
@@ -1,3 +1,21 @@
 // PLACE YOUR CODE HERE
 
+// Q1
+document.querySelectorAll('.emphasis').forEach((element) => {
+    element.style.fontFamily = 'Verdana';
+})
 
+// Q2
+document.querySelectorAll('li.finished').forEach((element) => {
+    element.style.textDecoration = 'line-through';
+})
+
+// Q3
+document.querySelectorAll('#low-priority li.todo').forEach((element) => {
+    element.style.backgroundColor = 'blue';
+})
+
+// Q4 
+document.querySelectorAll('.alert').forEach((element) => {
+    element.style.color = 'red';
+});
