be3fa7c4ef310f6ede572bef595a1cc5de73d38c
Wesley Cheah <wesley_188@yahoo.com>
2022-05-24 13:18:56 +0000

    Completed r2q3

--- r2q3_Transfer_Money/index.js
+++ r2q3_Transfer_Money/index.js
@@ -12,21 +12,41 @@ document.querySelector('#topUp10').addEventListener('click', function () {
 // Transfer $10 from account to wallet
 document.querySelector('#transfer10ToWallet').addEventListener('click', function () {
     let accountBalance = Number(account.innerHTML);
-    accountBalance -= 10;
-    account.innerHTML = accountBalance;
-    
     let walletBalance = Number(wallet.innerHTML);
-    walletBalance += 10;
+
+    // Ensure that there is enough balance in account
+    if (accountBalance >= 10) {
+        accountBalance -= 10;
+        walletBalance += 10;
+    }
+    
+    account.innerHTML = accountBalance;
     wallet.innerHTML = walletBalance;
 })
 
 // Transfer $10 from wallet to account
 document.querySelector('#transfer10ToAccount').addEventListener('click', function () {
     let accountBalance = Number(account.innerHTML);
-    accountBalance += 10;
-    account.innerHTML = accountBalance;
-    
     let walletBalance = Number(wallet.innerHTML);
-    walletBalance -= 10;
+    
+    // Ensure that there is enough balance in wallet
+    if (walletBalance >= 10) {
+        walletBalance -= 10;
+        accountBalance += 10;
+    }
+
+    wallet.innerHTML = walletBalance;
+    account.innerHTML = accountBalance;
+})
+
+// Spend money from wallet
+document.querySelector('#spend').addEventListener('click', function () {
+    let amount = Number(document.querySelector('#amount').value);
+    let walletBalance = Number(wallet.innerHTML);
+
+    // Ensure that there is enough balance in wallet
+    if (walletBalance >= amount) {
+        walletBalance -= amount;
+    }
     wallet.innerHTML = walletBalance;
 })
