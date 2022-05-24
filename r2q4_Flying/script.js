8b17d1ac8e2264b87d4a31e28c1163a8de2cfd00
Wesley Cheah <wesley_188@yahoo.com>
2022-05-24 14:14:25 +0000

    Completed r2q4

--- r2q4_Flying/script.js
+++ r2q4_Flying/script.js
@@ -1,3 +1,52 @@
 // PLACE YOUR CODE HERE
+function extractFormInfo()
+{
+  
+  let firstName = document.querySelector('#firstName').value;
+  
+  let lastName = document.querySelector('#lastName').value;
+  
+  let ticketClass = null;
+  let radioBtns = document.querySelectorAll('.ticketClass');
+  for (let radioBtn of radioBtns) {
+    if (radioBtn.checked) {
+      ticketClass = radioBtn.value;
+      break;
+    }
+  }
+  
+  let options = [];
+  let checkboxes = document.querySelectorAll('.options');
+  for (let checkbox of checkboxes) {
+    if (checkbox.checked) {
+      options.push(checkbox.value);
+    }
+  }
 
+  return [firstName, lastName, ticketClass, options];
+}
+
+document.querySelector('button').addEventListener('click', function () {
+  let info = extractFormInfo();
+  let ticketClass = info[2];
+  let options = info[3];
+
+  let totalCost = 0;
+  // Ticket cost
+  if (ticketClass == 'firstClass') {
+    totalCost += 1200;
+  }
+  else if (ticketClass == 'business') {
+    totalCost += 750;
+  }
+  else {
+    totalCost += 250;
+  }
+
+  // Add-on option cost
+  totalCost += options.length * 10;
+
+  document.querySelector('#summary').innerHTML = `Your choices are: Ticket class: ${ticketClass}, Options: ${options}, Total cost: $${totalCost}
+  `;
+})
 
