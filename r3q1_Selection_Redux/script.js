7f72b05de820e0e9c76e439a329f760e5749d2e3
Wesley Cheah <wesley_188@yahoo.com>
2022-05-25 04:40:20 +0000

    To continue

--- r3q1_Selection_Redux/script.js
+++ r3q1_Selection_Redux/script.js
@@ -1,3 +1,13 @@
 // PLACE YOUR CODE HERE
+// Q1
+let header2 = document.querySelector('h2');
+header2.style.fontSize = '32px';
+header2.style.fontFamily = 'Verdana';
 
+// Q2 
+let wolfImg = document.querySelector('#wolf-img');
+wolfImg.style.border = '1px solid black';
+wolfImg.style.padding = '32px 0 15px 0';
 
+// Q3
+let firstFact = document.querySelector('#randomFactList li:first-child');
